5eeadc14d711ba745c9c234e0164a05c3fbb0d87
erikbenton <erik.benton@hotmail.com>
2018-04-24 10:24:01 -0400

    adding emptyview for catalog list and FAB functionality

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.inventoryapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class CatalogActivity extends AppCompatActivity
 {
@@ -12,6 +17,24 @@ public class CatalogActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
+        // Find ListView to populate
+        ListView petListView = findViewById(R.id.list);
+
+        // Find and set EmptyView
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
