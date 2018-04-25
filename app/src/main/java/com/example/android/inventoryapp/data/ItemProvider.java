ebc2e3d9d56f1dc71ac37dff9e78f03f3335891a
erikbenton <erik.benton@hotmail.com>
2018-04-25 12:28:51 -0400

    filling out query for ItemProvider

--- app/src/main/java/com/example/android/inventoryapp/data/ItemProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ItemProvider.java
@@ -1,12 +1,16 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import com.example.android.inventoryapp.data.ItemContract.ItemEntry;
+import android.widget.CursorAdapter;
 
 public class ItemProvider extends ContentProvider
 {
@@ -32,14 +36,52 @@ public class ItemProvider extends ContentProvider
     @Override
     public boolean onCreate()
     {
-        return false;
+        mDbHelper = new ItemDbHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder)
     {
-        return null;
+        // Get readable database
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Make a cursor for holding the data
+        Cursor cursor;
+
+        // Get the Uri match
+        int match = sUriMatcher.match(uri);
+
+        // Use switch to determine what to do with the Uri
+        switch (match)
+        {
+            case ITEMS:
+                // Perform query to get all the items from the database
+                cursor = db.query(ItemEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case ITEM_ID:
+                // Set up the selection
+                selection = ItemEntry._ID + "=?";
+
+                // Get the Item ID from the Uri
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // Make query with given pet ID
+                cursor = db.query(ItemEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot open unknown URI " + uri);
+        }
+
+        // Set notification URI on the Cursor
+        // so that we knw what Content URI the Cursor was created for.
+        // If the data at this URI changes, then we know we need to update the cursor
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
     @Nullable
